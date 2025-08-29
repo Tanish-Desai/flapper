7bc81de8d66b75dbbec361858e7e58d79ed14257
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2025-08-29 20:28:55 +0530

    Added ground scroll.
    - Changed ground position in Ground.tscn to fix position errors while scrolling.
    - Fixed Resolution issue (Project->View Settings)
    - Added Ground scroll code

--- scenes/main.gd
+++ scenes/main.gd
@@ -13,6 +13,7 @@ const PIPE_RANGE : int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	screen_size = get_window().size
 	new_game()
 
 func new_game():
@@ -41,4 +42,9 @@ func start_game():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if game_running:
+		scroll += SCROLL_SPEED
+		if scroll >= screen_size.x:
+			scroll = 0
+		# Ground position.x is relative to position that has been manually set in scene editor
+		$Ground.position.x = -scroll
