f1adf5a637e8b8fb0b217e91c53e093281ab79ed
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2025-08-30 22:15:53 +0530

    Created Score area and signal

--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal hit
+signal scored
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,3 +15,6 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	hit.emit()
+
+func _on_score_area_body_entered(body: Node2D) -> void:
+	scored.emit()
